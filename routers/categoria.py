2fd5a36e4b7c4c9a4c5be4ca27e4a759069ab337
joaofelipemx <joao.teodoro@escolar.ifrn.edu.br>
2026-07-13 17:10:39 -0300

    feat: criação dos routers da categoria

--- routers/categoria.py
+++ routers/categoria.py
@@ -15,3 +15,25 @@ async def criar_categoria(categoria: schemas.CategoriaCreate, db: AsyncSession =
 @router.get("/", response_model=list[schemas.CategoriaResponse])
 async def listar_categorias(db: AsyncSession = Depends(get_db_connection)):
     return await crud.listar_categorias(db)
+
+@router.get("/{categoria_id}", response_model=schemas.CategoriaResponse)
+async def buscar_categoria(categoria_id: int, db: AsyncSession = Depends(get_db_connection)):
+    categoria = await crud.buscar_categoria(db, categoria_id)
+    if categoria is None:
+        raise HTTPException(status_code=404, datail="Categoria não encontrada.")
+    return categoria
+
+@router.put("/{categoria_id}", response_model=schemas.CategoriaResponse)
+async def atualizar_categoria(categoria_id: int, dados: schemas.CategoriaResponse, db: AsyncSession = Depends(get_db_connection)):
+    categoria = await crud.atualizar_categoria(db, categoria_id, dados)
+    if categoria is None:
+        raise HTTPException(status_code=404, datail="Categoria não encontrada.")
+    return categoria
+
+@router.delete("/{categoria_id}")
+async def deletar_categoria(categoria_id: int, db: AsyncSession = Depends(get_db_connection)):
+    categoria = await crud.deletar_cateria(db, categoria_id)
+    if categoria is None:
+        raise HTTPException(status_code=404, detail="Categoria não encontrada")
+    return categoria
+    
